52cd2c0df07caea53373f8fc25e5e071fc109a03
Dan Cross <cross@gajendra.net>
2024-10-18 00:40:39 +0000

    rust: clippy lints and minor cleanups
    Signed-off-by: Dan Cross <cross@gajendra.net>

--- fdpass-rs/src/main.rs
+++ fdpass-rs/src/main.rs
@@ -4,20 +4,18 @@ use libc::{iovec, msghdr, recvmsg, sendmsg};
 use libc::{AF_UNIX, SCM_RIGHTS, SOCK_STREAM, SOL_SOCKET};
 use libc::{CMSG_DATA, CMSG_FIRSTHDR, CMSG_LEN, CMSG_NXTHDR, CMSG_SPACE};
 
+#[cfg(not(target_env = "gnu"))]
+type MsgLen = libc::socklen_t;
 #[cfg(target_env = "gnu")]
 type MsgLen = libc::size_t;
-#[cfg(not(target_env = "gnu"))]
-type MsgLen = libc::c_uint;
 
 #[derive(Clone, Copy, Debug)]
 pub struct Sock(c_int);
 
 pub fn socks() -> (Sock, Sock) {
     let mut sds = [0, 0];
-    unsafe {
-        if socketpair(AF_UNIX, SOCK_STREAM, 0, sds.as_mut_ptr()) < 0 {
-            panic!("socketpair failed: {}", std::io::Error::last_os_error());
-        }
+    if unsafe { socketpair(AF_UNIX, SOCK_STREAM, 0, sds.as_mut_ptr()) } < 0 {
+        panic!("socketpair failed: {}", std::io::Error::last_os_error());
     }
     (Sock(sds[0]), Sock(sds[1]))
 }
@@ -29,23 +27,23 @@ pub fn sendfd(fd: c_int, sd: Sock) {
     let space_len = space.len();
     let mut dummy = 1u8;
     let mut iov = iovec {
-        iov_base: &raw mut dummy as *mut c_void,
+        iov_base: &mut dummy as *mut u8 as *mut c_void,
         iov_len: 1,
     };
     let zmh: msghdr = unsafe { std::mem::zeroed() };
-    let mut mh = msghdr {
+    let mh = msghdr {
         msg_iov: &mut iov,
         msg_iovlen: 1,
-        msg_control: space.as_mut_ptr() as *mut c_void,
+        msg_control: space.as_mut_ptr().cast(),
         msg_controllen: space_len as MsgLen,
         ..zmh
     };
-    let cmsg = unsafe { &mut *CMSG_FIRSTHDR(&mut mh) };
+    let cmsg = unsafe { &mut *CMSG_FIRSTHDR(&mh) };
     cmsg.cmsg_len = unsafe { CMSG_LEN(C_INT_SIZE) } as MsgLen;
     cmsg.cmsg_level = SOL_SOCKET;
     cmsg.cmsg_type = SCM_RIGHTS;
     unsafe {
-        let dp = CMSG_DATA(cmsg) as *mut c_int;
+        let dp = CMSG_DATA(cmsg).cast();
         std::ptr::write(dp, fd);
         if sendmsg(sd.0, &mh, 0) < 0 {
             panic!("sendmsg failed: {}", std::io::Error::last_os_error())
@@ -58,18 +56,18 @@ pub fn recvfd(sd: Sock) -> c_int {
     let space_len = space.len();
     let mut dummy = 0u8;
     let mut iov = iovec {
-        iov_base: &raw mut dummy as *mut c_void,
+        iov_base: &mut dummy as *mut u8 as *mut c_void,
         iov_len: 1,
     };
     let zmh: msghdr = unsafe { std::mem::zeroed() };
     let mut mh = msghdr {
         msg_iov: &mut iov,
         msg_iovlen: 1,
-        msg_control: space.as_mut_ptr() as *mut c_void,
+        msg_control: space.as_mut_ptr().cast(),
         msg_controllen: space_len as MsgLen,
         ..zmh
     };
-    let cmsg = unsafe { &mut *CMSG_FIRSTHDR(&mut mh) };
+    let cmsg = unsafe { &mut *CMSG_FIRSTHDR(&mh) };
     cmsg.cmsg_len = unsafe { CMSG_LEN(C_INT_SIZE) } as MsgLen;
     cmsg.cmsg_level = SOL_SOCKET;
     cmsg.cmsg_type = SCM_RIGHTS;
@@ -86,7 +84,7 @@ pub fn recvfd(sd: Sock) -> c_int {
         panic!("read bad data");
     }
     unsafe {
-        let dp = CMSG_DATA(cmsg) as *mut c_int;
+        let dp = CMSG_DATA(cmsg).cast();
         std::ptr::read(dp)
     }
 }
@@ -97,5 +95,7 @@ fn main() {
     let n = recvfd(rcv);
     println!("snd={snd:?}, rcv={rcv:?}, n={n}");
     let msg = "Hello, World!\n";
-    unsafe { libc::write(n, msg.as_ptr() as *const c_void, msg.len()); } 
+    unsafe {
+        libc::write(n, msg.as_ptr().cast(), msg.len());
+    }
 }
