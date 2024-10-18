cc69660daf729f9ca5c1b34a1dee06f0e7b173e4
Dan Cross <cross@gajendra.net>
2024-10-18 12:49:30 +0000

    rust: proper error checking
    Signed-off-by: Dan Cross <cross@gajendra.net>

--- fdpass-rs/src/main.rs
+++ fdpass-rs/src/main.rs
@@ -5,6 +5,8 @@ use libc::{AF_UNIX, SCM_RIGHTS, SOCK_STREAM, SOL_SOCKET};
 use libc::{CMSG_DATA, CMSG_FIRSTHDR, CMSG_LEN, CMSG_NXTHDR, CMSG_SPACE};
 use libc::{MSG_CTRUNC, MSG_TRUNC};
 
+use std::io::Error;
+
 #[cfg(not(target_env = "gnu"))]
 type MsgLen = libc::socklen_t;
 #[cfg(target_env = "gnu")]
@@ -13,24 +15,26 @@ type MsgLen = libc::size_t;
 #[derive(Clone, Copy, Debug)]
 pub struct Sock(c_int);
 
+pub type Result<T> = std::result::Result<T, Error>;
+
 #[repr(C)]
 union CMsgInt {
     _align: cmsghdr,
     space: [u8; unsafe { CMSG_SPACE(C_INT_SIZE) } as usize],
 }
 
-pub fn socks() -> (Sock, Sock) {
+pub fn socks() -> Result<(Sock, Sock)> {
     let mut sds = [0, 0];
     if unsafe { socketpair(AF_UNIX, SOCK_STREAM, 0, sds.as_mut_ptr()) } < 0 {
-        panic!("socketpair failed: {}", std::io::Error::last_os_error());
+        return Err(Error::last_os_error());
     }
-    (Sock(sds[0]), Sock(sds[1]))
+    Ok((Sock(sds[0]), Sock(sds[1])))
 }
 
 const C_INT_SIZE: u32 = std::mem::size_of::<c_int>() as u32;
 const ZMSGHDR: msghdr = unsafe { std::mem::MaybeUninit::zeroed().assume_init() };
 
-pub fn sendfd(fd: c_int, sd: Sock) {
+pub fn sendfd(fd: c_int, sd: Sock) -> Result<()> {
     let mut space = std::mem::MaybeUninit::<CMsgInt>::zeroed();
     let space = space.as_mut_ptr();
     let mut dummy = 1u8;
@@ -55,11 +59,12 @@ pub fn sendfd(fd: c_int, sd: Sock) {
     }
     #[cfg(not(miri))]
     if unsafe { sendmsg(sd.0, &mh, 0) } < 0 {
-        panic!("sendmsg failed: {}", std::io::Error::last_os_error())
+        return Err(Error::last_os_error())
     }
+    Ok(())
 }
 
-pub fn recvfd(sd: Sock) -> c_int {
+pub fn recvfd(sd: Sock) -> Result<c_int> {
     let mut space = std::mem::MaybeUninit::<CMsgInt>::zeroed();
     let space = space.as_mut_ptr();
     let mut dummy = 0u8;
@@ -77,34 +82,34 @@ pub fn recvfd(sd: Sock) -> c_int {
     let cmsg = unsafe { &mut *CMSG_FIRSTHDR(&mh) };
     #[cfg(not(miri))]
     if unsafe { recvmsg(sd.0, &mut mh, 0) } < 0 {
-        panic!("sendmsg failed: {}", std::io::Error::last_os_error());
+        return Err(Error::last_os_error());
     }
     if mh.msg_flags & (MSG_TRUNC | MSG_CTRUNC) != 0 {
-        panic!("msg truncated");
+        return Err(Error::other("recvfd: message truncated"));
     }
     if cmsg.cmsg_len != unsafe { CMSG_LEN(C_INT_SIZE) } as MsgLen {
-        panic!("wrong size");
+        return Err(Error::other("recvfd: wrong control message size"));
     }
     if cmsg.cmsg_level != SOL_SOCKET || cmsg.cmsg_type != SCM_RIGHTS {
-        panic!("bad message metadata");
+        return Err(Error::other("recvfd: bad message metadata"));
     }
     if dummy != 1 {
-        panic!("read bad data");
+        return Err(Error::other("recvfd: read unexpected message data"));
     }
     let next_hdr = unsafe { CMSG_NXTHDR(&mh, cmsg) };
     if !next_hdr.is_null() {
-        panic!("more than one control message");
+        return Err(Error::other("recvfd: extra control message"));
     }
     unsafe {
         let dp = CMSG_DATA(cmsg).cast();
-        std::ptr::read(dp)
+        Ok(std::ptr::read(dp))
     }
 }
 
 fn main() {
-    let (snd, rcv) = socks();
-    sendfd(1, snd);
-    let n = recvfd(rcv);
+    let (snd, rcv) = socks().expect("socketpair");
+    sendfd(1, snd).expect("sendfd");
+    let n = recvfd(rcv).expect("recvfd");
     println!("snd={snd:?}, rcv={rcv:?}, n={n}");
     let msg = "Hello, World!\n";
     unsafe {
